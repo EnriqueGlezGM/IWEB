445e3135e888dbf6bafca0eee477840f64decaa2
Enrique González <31137081+EnriqueGlezM@users.noreply.github.com>
2021-08-23 14:11:15 +0200

    Add files via upload

---
+++ App Pokedex/Model/Race.swift
@@ -0,0 +1,22 @@
+//
+//  Race.swift
+//  Pokedex
+//
+//  Created by Santiago Pavón on 26/11/14.
+//  Copyright (c) 2014 UPM. All rights reserved.
+//
+
+import Foundation
+
+/// Estructura para representar una raza de pokemon.
+struct Race {
+    
+    // Codigo de la raza.
+    let code: Int
+    
+    // Nombre de la raza.
+    let name: String
+    
+    // Nombre del fichero con el icono de la raza.
+    let icon: String
+}
